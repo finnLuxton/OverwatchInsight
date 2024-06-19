512ffe1f0ea45b005c69c559dcb30bb0488c7356
Finn Luxton <finlayluxton@gmail.com>
2024-06-20 11:16:37 +1200

    Up to date

---
+++ OverwatchInsight.Controller.Tests/HeroControllerTests.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Moq;
+using OverwatchInsight.Application.Contracts.Outbound;
+using OverwatchInsight.Controllers;
+using OverwatchInsight.Models;
+using Xunit;
+
+namespace OverwatchInsight.Tests
+{
+    public class HeroControllerTests
+    {
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<IHeroServiceProvider> _mockHeroServiceProvider;
+        private readonly HeroController _controller;
+
+        public HeroControllerTests()
+        {
+            _mockMapper = new Mock<IMapper>();
+            _mockHeroServiceProvider = new Mock<IHeroServiceProvider>();
+            _controller = new HeroController(_mockMapper.Object, _mockHeroServiceProvider.Object);
+        }
+
+        //[Fact]
+        //public async Task GivenValidInput_WhenCallingGetHeroAsync_GetHeroMatchupOutput()
+        //{
+        //    // Arrange
+        //    var enemyHeroes = new List<string> { "Reinhardt", "Zarya" };
+        //    var expectedHeroName = "Torbjorn";
+        //    var heroMatchup = new HeroMatchup(expectedHeroName, 0);
+
+        //    _mockHeroServiceProvider.Setup(s => s.GetHeroMatchup(It.IsAny<List<string>>()))
+        //        .Returns(Task.FromResult(heroMatchup));
+
+        //    // Act
+        //    var result = await _controller.GetHeroAsync(enemyHeroes);
+
+        //    // Assert
+        //    Assert.Equal(expectedHeroName, result);
+        //}
+    }
+}
