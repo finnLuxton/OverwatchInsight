40b45f30612e3c546aff06dfb078e5f4c2479312
Finn Luxton <finlayluxton@gmail.com>
2024-06-23 21:18:10 +1200

    Service matchup logic
    Service matchup logic

--- OverwatchInsight.Application/Services/HeroService.cs
+++ OverwatchInsight.Application/Services/HeroService.cs
@@ -1,9 +1,4 @@
 ﻿using OverwatchInsight.Application.Contracts.Inbound;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OverwatchInsight.Application.Models;
 using OverwatchInsight.Application.Contracts.Outbound;
 
@@ -20,35 +15,34 @@ public class HeroService : IHeroServiceProvider
 
     public async Task<List<HeroMatchup>> GetHeroMatchup(List<String> EnemyHeroes)
     {
-        // Outputs list of all heroes, and their matchup scores.
         var heroInformation = await _heroInformationProvider.GetHeroInformation();
 
         var heroMatchups = new List<HeroMatchup>();
 
-        //string HeroName,
-        //List<string> StrongAgainst,
-        //List<string> GoodAgainst,
-        //List<string> WeakAgainst,
-        //List<string> BadAgainst
-
         // TODO Extract this out to a function.
         foreach (HeroInformation hero in heroInformation)
         {
+            int matchupValue = 0;
+
+            foreach (var enemyHero in EnemyHeroes)
+            { // TODO: Refine these matchup values. Not much thought put into these yet
+                if (hero.StrongAgainst.Contains(enemyHero))
+                    matchupValue += 5;
+
+                if (hero.GoodAgainst.Contains(enemyHero))
+                    matchupValue += 2;
+
+                if (hero.WeakAgainst.Contains(enemyHero))
+                    matchupValue += -2;
+
+                if (hero.BadAgainst.Contains(enemyHero))
+                    matchupValue += -5;
+            }
+
             heroMatchups.Add(new HeroMatchup(
-                                    HeroName: hero.HeroName,
-                                    MatchupValue: 0));
-
+                        HeroName: hero.HeroName,
+                        MatchupValue: matchupValue));
         }
-
-        // For each hero in JsonFileOutput
-        // // For each against attribute
-        // // // Check if agaisnt attribute is empty
-        // // // If not empty, iterate through EnemyHeroes parameter.
-        // // // // If EnemyHeroes exists within attribute, adjust HeroMatchup.MatchupScore value
-
-        // E.G We have an enemy hero parameter of Winston.
-        // The JsonHeroMatchup for Reaper has Winston as StrongAgaisnt, and Genji as BadAgaisnt, and Soldier 76 as nothing.
-        // The JsonHeroMatchup will give Reaper +5 Matchup score, and Genji -5, and Soldier has no change.
 
         return heroMatchups;
     }
