8291dc7a59da9131802413f72eb08c9952181609
Finn Luxton <finlayluxton@gmail.com>
2024-06-23 21:01:14 +1200

    Python scripts. Updated service
    Added python scripts for generating json file for matchup values
    
    Updated service to build a matchup list not on hardcoded values, but based on the script given. Should support future hero additions without build changes

--- OverwatchInsight.Application/Services/HeroService.cs
+++ OverwatchInsight.Application/Services/HeroService.cs
@@ -18,20 +18,39 @@ public class HeroService : IHeroServiceProvider
         _heroInformationProvider = heroInformationProvider ?? throw new ArgumentNullException(nameof(heroInformationProvider));
     }
 
-    public async Task<List<HeroMatchup>> GetHeroMatchup(List<String> Heroes)
+    public async Task<List<HeroMatchup>> GetHeroMatchup(List<String> EnemyHeroes)
     {
-        var heroMatchupList = new List<HeroMatchup>
-        {
-            new HeroMatchup("Torbjorn", 0),
-            new HeroMatchup("Ashe", -5)
-        };
-
         // Outputs list of all heroes, and their matchup scores.
-        var output = await _heroInformationProvider.GetHeroInformation();
+        var heroInformation = await _heroInformationProvider.GetHeroInformation();
 
-        // TODO Based off user inputted heroes, return a list of heroes and their aggregated matchup scores
+        var heroMatchups = new List<HeroMatchup>();
 
-        return heroMatchupList;
+        //string HeroName,
+        //List<string> StrongAgainst,
+        //List<string> GoodAgainst,
+        //List<string> WeakAgainst,
+        //List<string> BadAgainst
+
+        // TODO Extract this out to a function.
+        foreach (HeroInformation hero in heroInformation)
+        {
+            heroMatchups.Add(new HeroMatchup(
+                                    HeroName: hero.HeroName,
+                                    MatchupValue: 0));
+
+        }
+
+        // For each hero in JsonFileOutput
+        // // For each against attribute
+        // // // Check if agaisnt attribute is empty
+        // // // If not empty, iterate through EnemyHeroes parameter.
+        // // // // If EnemyHeroes exists within attribute, adjust HeroMatchup.MatchupScore value
+
+        // E.G We have an enemy hero parameter of Winston.
+        // The JsonHeroMatchup for Reaper has Winston as StrongAgaisnt, and Genji as BadAgaisnt, and Soldier 76 as nothing.
+        // The JsonHeroMatchup will give Reaper +5 Matchup score, and Genji -5, and Soldier has no change.
+
+        return heroMatchups;
     }
 }
 
